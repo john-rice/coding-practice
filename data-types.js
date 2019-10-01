54aa5ff7e1a2b573e5fa919de75e7fc8f074c5e1
John rice <john_rice@oxfordcorp.com>
2019-09-30 21:14:15 -0400

    Show examples of data types

---
+++ data-types.js
@@ -0,0 +1,14 @@
+var myNum = 8;
+var mNum = 9;
+let name = 'Jeff';
+let myObj = {};
+let me = {
+    name: 'Jeff',
+    age: 12
+}
+
+var myArray = []
+var myBigArray = ['first', 'last']
+
+var today = new Date()
+var xMas2018 = new Date('2018-12-25')
