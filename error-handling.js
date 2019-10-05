bc2acae6628fa1471cd2a7d866c91df0a8923af1
John Rice <jrice@labact.com>
2019-10-05 10:29:13 -0400

    Display basics of error handling with try/catch

---
+++ error-handling.js
@@ -0,0 +1,14 @@
+
+
+try {
+    // this is the code that will be handled if an error occurs
+    var x = y
+
+} catch (errorDetail) {
+    // this block will run when the block of 'try' hits an error
+    // per slide 8, we have properties of the errorDetail object
+    // mesage, name and stack
+    console.log(errorDetail.message)
+    console.log(errorDetail.name)
+    //console.log(errorDetail.stack)
+}
